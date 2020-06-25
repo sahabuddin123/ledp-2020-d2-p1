680fef4180ea4dd4bd43ff13254a7912839ea16b
sahabuddin <sahabuddinriyaj984@gmail.com>
2020-06-25 16:04:20 +0600

    news section and testimonial section complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+
+
+$('.testi').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
@@ -16,4 +36,4 @@ $('.owl-carousel').owlCarousel({
             items:3
         }
     }
-})
+});
